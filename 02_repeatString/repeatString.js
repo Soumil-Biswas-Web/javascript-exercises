ece1e63ccd0cc7f6a20972ce5e782687f6f89c5e
Soumil-Biswas-Web <soumil.biswas19@hotmail.com>
2024-03-07 15:43:00 +0530

    Completed Excercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, repeatNumber) {
+    if (repeatNumber < 0) return 'ERROR';
+    let outputString = '';
+    for(let i = 0; i<repeatNumber; i++){
+        outputString = outputString + inputString;
+    }
+    return outputString;
 };
 
 // Do not edit below this line
