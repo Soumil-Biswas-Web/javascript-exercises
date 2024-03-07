7de5bec2c13393efe88c10a78a12c4c509d0fab9
Soumil-Biswas-Web <soumil.biswas19@hotmail.com>
2024-03-07 16:26:32 +0530

    Completed Excercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...itemsToBeRemoved) {
+    for(const arg of itemsToBeRemoved){
+        for(let i=0; i<inputArray.length; i++){
+            if(arg === inputArray[i]){
+                inputArray.splice(i, 1);
+                i = 0;
+            }
+        }
+    }
+    return inputArray;
 };
 
 // Do not edit below this line
