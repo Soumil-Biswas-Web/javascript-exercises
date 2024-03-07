a70b990d7b09475c921f2925688acd3db8b2b9d9
Soumil-Biswas-Web <soumil.biswas19@hotmail.com>
2024-03-07 16:37:19 +0530

    Completed Excercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(intA, intB) {
+    let total = 0;
+    let temp = 0;
+    if (intA > intB){
+        temp = intB;
+        intB = intA;
+        intA = temp;
+    }
+    if (intA < 0 || intB < 0 || typeof intA != 'number' || typeof intB != 'number'){
+        return 'ERROR';
+    }
+    for(let i=intA; i<intB+1; i++){
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
