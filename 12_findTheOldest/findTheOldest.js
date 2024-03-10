3a1355bf5a5e599ad0cffd6505b6531ecd9adda0
Soumil Biswas <soumil.biswas19@gmail.com>
2024-03-10 08:40:35 -0700

    Completed Exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+function getAge(yearOfBirth, yearOfDeath){
+    if(!yearOfDeath){
+        yearOfDeath = new Date().getFullYear();
+    }
+    return yearOfDeath - yearOfBirth;
+}
 
+const findTheOldest = function(people) {
+    const maxAge = people.reduce((oldest, current) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        if (oldestAge > currentAge){
+            return oldest;
+        }
+        else return current;
+    });
+    return maxAge;
 };
 
 // Do not edit below this line
