5257f1ef235b9ab16917970731cbf3f375986b42
Soumil Biswas <soumil.biswas19@gmail.com>
2024-03-10 06:22:53 -0700

    Completed Exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const dePunct = string.replace(/[^\w]|_/g, ''); //removes punctuations and white space
+    const lowercase = dePunct.toLowerCase();
+    const strArray = lowercase.split("");
+    let mid = Math.floor(strArray.length/2);
+    let end = strArray.length-1;
+    for (let i=0; i<=mid; i++){
+        if (strArray[i] !== strArray[end-i]){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
