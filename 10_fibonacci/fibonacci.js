f981fe845dbf16b6aac67bc1056f0c36c268d577
Soumil Biswas <soumil.biswas19@gmail.com>
2024-03-10 06:57:56 -0700

    Completed Exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let input = Number(n);
+    let a = 0;
+    let b = 1;
+    let temp = 0;
+    if (input < 0){
+        return "OOPS";
+    }
+    else if(input == 0){
+        return a;
+    }
+    else if(input > 1){        
+        for (let i=2; i<input+1; i++){
+            temp = b;
+            b = a + b;
+            a = temp;
+        }    
+    }
+    return b; 
 };
 
 // Do not edit below this line
